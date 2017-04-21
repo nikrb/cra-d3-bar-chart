ea5ab83fb88d74c860be940fb8bcf66e2bbe6ae6
nikrb <nik_rb@yahoo.com>
2017-04-21 22:31:07 +0100

    add axes

--- src/BarChart.js
+++ src/BarChart.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import DataSeries from './DataSeries';
 import d3 from 'd3';
+import XYAxis from './XYAxis';
 
 export default class BarChart extends React.Component {
   render = () => {
+    const padding = 30;
     var yScale = d3.scale.linear()
-      .domain([0, d3.max(this.props.data)])
-      .range([0, this.props.height]);
+      .domain([d3.max(this.props.data), 0])
+      .range([padding, this.props.height-padding]);
 
     var xScale = d3.scale.ordinal()
       .domain(d3.range(this.props.data.length))
-      .rangeRoundBands([0, this.props.width], 0.05);
+      .rangeRoundBands([padding, this.props.width-padding], 0.05);
 
     return (
       <svg width={this.props.width} height={this.props.height} >
@@ -21,6 +23,14 @@ export default class BarChart extends React.Component {
             width={this.props.width}
             height={this.props.height}
             colour="cornflowerblue"
+          />
+          <XYAxis
+            xScale={xScale}
+            yScale={yScale}
+            data={this.props.data}
+            width={this.props.width}
+            height={this.props.height}
+            padding={padding}
           />
       </svg>
     );
--- src/DataSeries.js
+++ src/DataSeries.js
@@ -6,12 +6,11 @@ export default class DataSeries extends React.Component {
     const { data, xScale, yScale, height, colour } = this.props;
 
     var bars = data.map(function(point, i) {
-      // width={xScale.rangeBand()}
       return (
         <Bar height={yScale(point)} width={xScale.rangeBand()}
           offset={xScale(i)} availableHeight={height}
           colour={colour} key={i} />
-      )
+      );
     });
 
     return (
